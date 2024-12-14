0432d85f80d1b0f4104753ca75d622d9a006edcf
Potter??? <103591534+Dandorime@users.noreply.github.com>
2024-12-15 00:50:56 +0300

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,13 +14,36 @@ import FlyonuiScript from '@/components/FlyonuiScript';
 export const metadata: Metadata = {
   title: 'Your Application Title Goes Here',
   description: 'Your application description goes here',
-};
+};    
+// import { Telegraf, Markup } from 'telegraf'
+// import { message } from 'telegraf/filters'
 
-export default async function RootLayout({ children }: PropsWithChildren) {
-  const locale = await getLocale();
+
+// export default async function RootLayout({ children }: PropsWithChildren) {
+//   const locale = await getLocale();
+
+  
+// const token = '7673698115:AAFSWRVd_U4_mZkoiKHXxUhtX00zOBjL4LM'
+// const webAppUrl = 'https://bring-back-my.vercel.app/'
+
+// const bot = new Telegraf(token)
+
+// bot.command('start', (ctx) => {
+//   ctx.reply(
+//     'Добро пожаловать! Нажмите на кнопку ниже, чтобы запустить приложение',
+//     Markup.keyboard([
+//       Markup.button.webApp('Отправить сообщение', `${webAppUrl}`),
+//     ])
+//   )
+// })
+
+// bot.launch()
 
   return (
     <html lang={locale}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"></meta>
+      </head>
     <body>
       <I18nProvider>
         <Root>
